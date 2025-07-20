ddcf5c2726c544d3fde98abac7d1abe675416921
mwatts <mwatts@users.noreply.github.com>
2025-07-20 12:04:43 +0000

    deploy: 5f40b9df3671bdaf591ff27af789f0247cabf322

---
+++ toc.js
@@ -0,0 +1,70 @@
+// Populate the sidebar
+//
+// This is a script, and not included directly in the page, to control the total size of the book.
+// The TOC contains an entry for each page, so if each page includes a copy of the TOC,
+// the total size of the page becomes O(n**2).
+class MDBookSidebarScrollbox extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="introduction.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded "><a href="getting-started.html"><strong aria-hidden="true">2.</strong> Getting Started</a></li><li class="chapter-item expanded "><a href="contributing.html"><strong aria-hidden="true">3.</strong> Developing Locally</a></li><li class="chapter-item expanded "><a href="architecture.html"><strong aria-hidden="true">4.</strong> Architecture</a></li><li class="chapter-item expanded "><a href="query-language.html"><strong aria-hidden="true">5.</strong> Query Language</a></li><li class="chapter-item expanded "><a href="incremental-queries.html"><strong aria-hidden="true">6.</strong> Incremental Queries</a></li><li class="chapter-item expanded "><a href="schemas.html"><strong aria-hidden="true">7.</strong> Schemas</a></li><li class="chapter-item expanded "><a href="repository-workflows.html"><strong aria-hidden="true">8.</strong> Repository Workflows</a></li><li class="chapter-item expanded "><a href="commit-selectors.html"><strong aria-hidden="true">9.</strong> Commit Selectors</a></li><li class="chapter-item expanded "><a href="deep-dive/index.html"><strong aria-hidden="true">10.</strong> Deep Dive</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="deep-dive/philosophy.html"><strong aria-hidden="true">10.1.</strong> Philosophy</a></li><li class="chapter-item expanded "><a href="deep-dive/identifiers.html"><strong aria-hidden="true">10.2.</strong> Identifiers</a></li><li class="chapter-item expanded "><a href="deep-dive/trible-structure.html"><strong aria-hidden="true">10.3.</strong> Trible Structure</a></li><li class="chapter-item expanded "><a href="pile-format.html"><strong aria-hidden="true">10.4.</strong> Pile Format</a></li></ol></li></ol>';
+        // Set the current, active page, and reveal it if it's hidden
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
+        if (current_page.endsWith("/")) {
+            current_page += "index.html";
+        }
+        var links = Array.prototype.slice.call(this.querySelectorAll("a"));
+        var l = links.length;
+        for (var i = 0; i < l; ++i) {
+            var link = links[i];
+            var href = link.getAttribute("href");
+            if (href && !href.startsWith("#") && !/^(?:[a-z+]+:)?\/\//.test(href)) {
+                link.href = path_to_root + href;
+            }
+            // The "index" page is supposed to alias the first chapter in the book.
+            if (link.href === current_page || (i === 0 && path_to_root === "" && current_page.endsWith("/index.html"))) {
+                link.classList.add("active");
+                var parent = link.parentElement;
+                if (parent && parent.classList.contains("chapter-item")) {
+                    parent.classList.add("expanded");
+                }
+                while (parent) {
+                    if (parent.tagName === "LI" && parent.previousElementSibling) {
+                        if (parent.previousElementSibling.classList.contains("chapter-item")) {
+                            parent.previousElementSibling.classList.add("expanded");
+                        }
+                    }
+                    parent = parent.parentElement;
+                }
+            }
+        }
+        // Track and set sidebar scroll position
+        this.addEventListener('click', function(e) {
+            if (e.target.tagName === 'A') {
+                sessionStorage.setItem('sidebar-scroll', this.scrollTop);
+            }
+        }, { passive: true });
+        var sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
+        sessionStorage.removeItem('sidebar-scroll');
+        if (sidebarScrollTop) {
+            // preserve sidebar scroll position when navigating via links within sidebar
+            this.scrollTop = sidebarScrollTop;
+        } else {
+            // scroll sidebar to current active section when navigating via "next/previous chapter" buttons
+            var activeSection = document.querySelector('#sidebar .active');
+            if (activeSection) {
+                activeSection.scrollIntoView({ block: 'center' });
+            }
+        }
+        // Toggle buttons
+        var sidebarAnchorToggles = document.querySelectorAll('#sidebar a.toggle');
+        function toggleSection(ev) {
+            ev.currentTarget.parentElement.classList.toggle('expanded');
+        }
+        Array.from(sidebarAnchorToggles).forEach(function (el) {
+            el.addEventListener('click', toggleSection);
+        });
+    }
+}
+window.customElements.define("mdbook-sidebar-scrollbox", MDBookSidebarScrollbox);
